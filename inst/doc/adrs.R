e7481574c1aa922afbaef3280feb6918f813db20
Stefan Bundfuss <stefan.bundfuss@roche.com>
2022-09-13 09:10:16 +0000

    version 0.1.0

---
+++ inst/doc/adrs.R
@@ -0,0 +1,482 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+library(admiraldev)
+
+## ----message=FALSE------------------------------------------------------------
+library(admiral)
+library(admiralonco)
+library(dplyr)
+library(admiral.test)
+library(lubridate)
+library(stringr)
+data("admiral_adsl")
+data("admiral_rs")
+data("admiral_tu")
+
+adsl <- admiral_adsl
+rs <- admiral_rs
+tu <- admiral_tu
+
+rs <- convert_blanks_to_na(rs)
+tu <- convert_blanks_to_na(tu)
+
+## ----echo=FALSE---------------------------------------------------------------
+rs <- filter(rs, USUBJID %in% c("01-701-1015", "01-701-1023", "01-703-1086", "01-703-1096", "01-707-1037", "01-716-1024"))
+tu <- filter(tu, USUBJID %in% c("01-701-1015", "01-701-1023", "01-703-1086", "01-703-1096", "01-707-1037", "01-716-1024"))
+adsl <- filter(adsl, USUBJID %in% c("01-701-1015", "01-701-1023", "01-703-1086", "01-703-1096", "01-707-1037", "01-716-1024"))
+
+## ----eval=TRUE----------------------------------------------------------------
+adsl_vars <- vars(RANDDT)
+adrs <- derive_vars_merged(
+  rs,
+  dataset_add = adsl,
+  new_vars = adsl_vars,
+  by_vars = vars(STUDYID, USUBJID)
+)
+
+## ---- eval=TRUE, echo=FALSE---------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, RSTESTCD, RSDTC, VISIT, RANDDT),
+  filter = RSTESTCD == "OVRLRESP"
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  filter(RSEVAL == "INVESTIGATOR" & RSTESTCD == "OVRLRESP") %>%
+  mutate(
+    PARAMCD = "OVR",
+    PARAM = "Overall Response by Investigator",
+    PARCAT1 = "Tumor Response",
+    PARCAT2 = "Investigator",
+    PARCAT3 = "Recist 1.1"
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, VISIT, RSTESTCD, RSEVAL, PARAMCD, PARAM, PARCAT1, PARCAT2, PARCAT3)
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_vars_dt(
+    dtc = RSDTC,
+    new_vars_prefix = "A",
+    highest_imputation = "D",
+    date_imputation = "last"
+  ) %>%
+  mutate(AVISIT = VISIT)
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, RSSTRESC, RSDTC, ADT, ADTF)
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  mutate(
+    AVALC = RSSTRESC,
+    AVAL = aval_resp(AVALC)
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, RSSTRESC, AVALC, AVAL)
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  restrict_derivation(
+    derivation = derive_var_extreme_flag,
+    args = params(
+      by_vars = vars(STUDYID, USUBJID, ADT),
+      order = vars(AVAL, RSSEQ),
+      new_var = ANL01FL,
+      mode = "last"
+    ),
+    filter = !is.na(AVAL) & ADT >= RANDDT
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, RANDDT, ANL01FL)
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  adrs <- adrs %>%
+#    mutate(
+#      ANL01FL = case_when(
+#        !is.na(AVAL) & ADT >= RANDDT & ADT < NACTDT ~ "Y",
+#        TRUE ~ NA_character_
+#      )
+#    )
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_first_event(
+    dataset_adsl = adsl,
+    dataset_source = adrs,
+    filter_source = PARAMCD == "OVR" & AVALC == "PD" & ANL01FL == "Y",
+    date_var = ADT,
+    set_values_to = vars(
+      PARAMCD = "PD",
+      PARAM = "Disease Progression by Investigator",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, ANL01FL),
+  filter = PARAMCD == "PD"
+)
+
+## -----------------------------------------------------------------------------
+pd <- date_source(
+  dataset_name = "adrs",
+  date = ADT,
+  filter = PARAMCD == "PD" & AVALC == "Y"
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  pd <- date_source(
+#    dataset_name = "adsl",
+#    date = PDDT
+#  )
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_response(
+    dataset_adsl = adsl,
+    filter_source = PARAMCD == "OVR" & AVALC %in% c("CR", "PR") & ANL01FL == "Y",
+    source_pd = pd,
+    source_datasets = list(adrs = adrs),
+    set_values_to = vars(
+      PARAMCD = "RSP",
+      PARAM = "Response by Investigator (confirmation not required)",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, ANL01FL),
+  filter = PARAMCD == "RSP"
+)
+
+## -----------------------------------------------------------------------------
+resp <- date_source(
+  dataset_name = "adrs",
+  date = ADT,
+  filter = PARAMCD == "RSP" & AVALC == "Y"
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_clinbenefit(
+    dataset_adsl = adsl,
+    filter_source = PARAMCD == "OVR" & ANL01FL == "Y",
+    source_resp = resp,
+    source_pd = pd,
+    source_datasets = list(adrs = adrs),
+    reference_date = RANDDT,
+    ref_start_window = 42,
+    set_values_to = vars(
+      PARAMCD = "CB",
+      PARAM = "Clinical Benefit by Investigator (confirmation for response not required)",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, RANDDT, ANL01FL),
+  filter = PARAMCD == "CB"
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_bor(
+    dataset_adsl = adsl,
+    filter_source = PARAMCD == "OVR" & ANL01FL == "Y",
+    source_pd = pd,
+    source_datasets = list(adrs = adrs),
+    reference_date = RANDDT,
+    ref_start_window = 42,
+    set_values_to = vars(
+      PARAMCD = "BOR",
+      PARAM = "Best Overall Response by Investigator (confirmation not required)",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, RANDDT, ANL01FL),
+  filter = PARAMCD == "BOR"
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  aval_resp_new <- function(arg) {
+#    case_when(
+#      arg == "CR" ~ 7,
+#      arg == "PR" ~ 6,
+#      arg == "SD" ~ 5,
+#      arg == "NON-CR/NON-PD" ~ 4,
+#      arg == "PD" ~ 3,
+#      arg == "NE" ~ 2,
+#      arg == "MISSING" ~ 1,
+#      TRUE ~ NA_real_
+#    )
+#  }
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_first_event(
+    dataset_adsl = adsl,
+    dataset_source = adrs,
+    filter_source = PARAMCD == "BOR" & AVALC %in% c("CR", "PR") & ANL01FL == "Y",
+    date_var = ADT,
+    set_values_to = vars(
+      PARAMCD = "BCP",
+      PARAM = "Best Overall Response of CR/PR by Investigator (confirmation not required)",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, ANL01FL),
+  filter = PARAMCD == "BCP"
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_confirmed_resp(
+    dataset_adsl = adsl,
+    filter_source = PARAMCD == "OVR" & AVALC %in% c("CR", "PR") & ANL01FL == "Y",
+    source_pd = pd,
+    source_datasets = list(adrs = adrs),
+    ref_confirm = 28,
+    set_values_to = vars(
+      PARAMCD = "CRSP",
+      PARAM = "Confirmed Response by Investigator",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+confirmed_resp <- date_source(
+  dataset_name = "adrs",
+  date = ADT,
+  filter = PARAMCD == "CRSP" & AVALC == "Y"
+)
+
+adrs <- adrs %>%
+  derive_param_clinbenefit(
+    dataset_adsl = adsl,
+    filter_source = PARAMCD == "OVR" & ANL01FL == "Y",
+    source_resp = confirmed_resp,
+    source_pd = pd,
+    source_datasets = list(adrs = adrs),
+    reference_date = RANDDT,
+    ref_start_window = 42,
+    set_values_to = vars(
+      PARAMCD = "CCB",
+      PARAM = "Confirmed Clinical Benefit by Investigator",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  ) %>%
+  derive_param_confirmed_bor(
+    dataset_adsl = adsl,
+    filter_source = PARAMCD == "OVR" & ANL01FL == "Y",
+    source_pd = pd,
+    source_datasets = list(adrs = adrs),
+    reference_date = RANDDT,
+    ref_start_window = 42,
+    ref_confirm = 28,
+    set_values_to = vars(
+      PARAMCD = "CBOR",
+      PARAM = "Best Confirmed Overall Response by Investigator",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  ) %>%
+  derive_param_first_event(
+    dataset_adsl = adsl,
+    dataset_source = adrs,
+    filter_source = PARAMCD == "CBOR" & AVALC %in% c("CR", "PR") & ANL01FL == "Y",
+    date_var = ADT,
+    set_values_to = vars(
+      PARAMCD = "CBCP",
+      PARAM = "Best Confirmed Overall Response of CR/PR by Investigator",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, RANDDT, ANL01FL),
+  filter = PARAMCD %in% c("CRSP", "CCB", "CBOR", "CBCP")
+)
+
+## -----------------------------------------------------------------------------
+adrsirf <- rs %>%
+  filter(RSEVAL == "INDEPENDENT ASSESSOR" & RSEVALID == "RADIOLOGIST 1" & RSTESTCD == "OVRLRESP") %>%
+  mutate(
+    PARAMCD = "OVRR1",
+    PARAM = "Overall Response by Radiologist 1",
+    PARCAT1 = "Tumor Response",
+    PARCAT2 = "Radiologist",
+    PARCAT3 = "Recist 1.1"
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrsirf,
+  display_vars = vars(USUBJID, VISIT, RSTESTCD, RSEVAL, PARAMCD, PARAM, PARCAT1, PARCAT2, PARCAT3),
+  filter = PARAMCD == "OVRR1"
+)
+
+## -----------------------------------------------------------------------------
+adsldth <- adsl %>%
+  select(STUDYID, USUBJID, DTHDT, !!!adsl_vars)
+
+adrs <- adrs %>%
+  derive_param_first_event(
+    dataset_adsl = adsldth,
+    dataset_source = adsldth,
+    filter_source = !is.na(DTHDT),
+    date_var = DTHDT,
+    set_values_to = vars(
+      PARAMCD = "DEATH",
+      PARAM = "Death",
+      PARCAT1 = "Reference Event",
+      ANL01FL = "Y"
+    )
+  ) %>%
+  select(-DTHDT)
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, ANL01FL),
+  filter = PARAMCD == "DEATH"
+)
+
+## -----------------------------------------------------------------------------
+adrs <- adrs %>%
+  derive_param_lasta(
+    filter_source = PARAMCD == "OVR" & ANL01FL == "Y",
+    set_values_to = vars(
+      PARAMCD = "LSTA",
+      PARAM = "Last Disease Assessment by Investigator",
+      PARCAT1 = "Tumor Response",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, ANL01FL),
+  filter = PARAMCD == "LSTA"
+)
+
+## -----------------------------------------------------------------------------
+adslmdis <- adsl %>%
+  select(STUDYID, USUBJID, !!!adsl_vars)
+
+adrs <- adrs %>%
+  derive_param_exist_flag(
+    dataset_adsl = adslmdis,
+    dataset_add = tu,
+    condition = TUEVAL == "INVESTIGATOR" & TUSTRESC == "TARGET" & VISIT == "BASELINE",
+    false_value = "N",
+    missing_value = "N",
+    set_values_to = vars(
+      PARAMCD = "MDIS",
+      PARAM = "Measurable Disease at Baseline by Investigator",
+      PARCAT2 = "Investigator",
+      PARCAT3 = "Recist 1.1",
+      ANL01FL = "Y"
+    )
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, AVISIT, PARAMCD, PARAM, AVALC, ADT, ANL01FL),
+  filter = PARAMCD == "MDIS"
+)
+
+## ----eval=TRUE----------------------------------------------------------------
+adrs <- adrs %>%
+  derive_var_obs_number(
+    by_vars = vars(STUDYID, USUBJID),
+    order = vars(PARAMCD, ADT, VISITNUM, RSSEQ),
+    check_type = "error"
+  )
+
+## ---- eval=TRUE, echo=FALSE---------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, PARAMCD, ADT, VISITNUM, AVISIT, ASEQ),
+  filter = USUBJID == "01-701-1015"
+)
+
+## ----eval=TRUE----------------------------------------------------------------
+adrs <- adrs %>%
+  derive_vars_merged(
+    dataset_add = select(adsl, !!!negate_vars(adsl_vars)),
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+## ---- eval=TRUE, echo=FALSE---------------------------------------------------
+dataset_vignette(
+  adrs,
+  display_vars = vars(USUBJID, RFSTDTC, RFENDTC, DTHDTC, DTHFL, AGE, AGEU),
+  filter = USUBJID == "01-701-1015"
+)
+
---
+++ inst/doc/adtte.R
@@ -0,0 +1,173 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+library(admiraldev)
+
+## ---- warning=FALSE, message=FALSE--------------------------------------------
+library(admiral)
+library(admiralonco)
+library(dplyr)
+library(admiral.test)
+library(lubridate)
+
+## -----------------------------------------------------------------------------
+data("admiral_adsl")
+data("admiral_adrs")
+adsl <- admiral_adsl
+adrs <- admiral_adrs
+
+## ----echo=FALSE---------------------------------------------------------------
+knitr::kable(list_tte_source_objects(package = "admiralonco"))
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  adsl_death_event <- event_source(
+#    dataset_name = "adsl",
+#    date = DTHDT,
+#    set_values_to = vars(
+#      EVNTDESC = "STUDY DEATH",
+#      SRCDOM = "ADSL",
+#      SRCVAR = "DTHDT"
+#    )
+#  )
+
+## -----------------------------------------------------------------------------
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = adrs,
+    filter_add = PARAMCD == "RSP" & AVALC == "Y" & ANL01FL == "Y",
+    by_vars = vars(STUDYID, USUBJID),
+    new_vars = vars(TEMP_RESPDT = ADT)
+  )
+
+adrs <- adrs %>%
+  derive_vars_merged(
+    dataset_add = adsl,
+    by_vars = vars(STUDYID, USUBJID),
+    new_vars = vars(TEMP_RESPDT)
+  )
+
+## -----------------------------------------------------------------------------
+adtte <- derive_param_tte(
+  dataset_adsl = adsl,
+  start_date = RANDDT,
+  event_conditions = list(death_event),
+  censor_conditions = list(lastalive_censor, rand_censor),
+  source_datasets = list(adsl = adsl, adrs = adrs),
+  set_values_to = vars(PARAMCD = "OS", PARAM = "Overall Survival")
+) %>%
+  derive_param_tte(
+    dataset_adsl = adsl,
+    start_date = RANDDT,
+    event_conditions = list(pd_event, death_event),
+    censor_conditions = list(lasta_censor, rand_censor),
+    source_datasets = list(adsl = adsl, adrs = adrs),
+    set_values_to = vars(PARAMCD = "PFS", PARAM = "Progression Free Survival")
+  ) %>%
+  derive_param_tte(
+    dataset_adsl = adsl,
+    start_date = TEMP_RESPDT,
+    event_conditions = list(pd_event, death_event),
+    censor_conditions = list(lasta_censor),
+    source_datasets = list(adsl = filter(adsl, !is.na(TEMP_RESPDT)), adrs = filter(adrs, !is.na(TEMP_RESPDT))),
+    set_values_to = vars(PARAMCD = "RSD", PARAM = "Duration of Response")
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adtte,
+  display_vars = vars(USUBJID, PARAMCD, PARAM, STARTDT, ADT, CNSR)
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  pd_nact_event <- event_source(
+#    dataset_name = "adsl",
+#    filter = PDDT < NACTDT | is.na(NACTDT),
+#    date = PDDT,
+#    set_values_to = vars(
+#      EVNTDESC = "Disease Progression prior to NACT",
+#      SRCDOM = "ADSL",
+#      SRCVAR = "PDDT"
+#    )
+#  )
+#  
+#  death_nact_event <- event_source(
+#    dataset_name = "adsl",
+#    filter = DTHDT < NACTDT | is.na(NACTDT),
+#    date = DTHDT,
+#    set_values_to = vars(
+#      EVNTDESC = "Death prior to NACT",
+#      SRCDOM = "ADSL",
+#      SRCVAR = "DTHDT"
+#    )
+#  )
+#  
+#  lasta_nact_censor <- censor_source(
+#    dataset_name = "adsl",
+#    date = LASTANDT,
+#    set_values_to = vars(
+#      EVNTDESC = "Last Tumor Assessment prior to NACT",
+#      CNSDTDSC = "Last Tumor Assessment prior to NACT",
+#      SRCDOM = "ADSL",
+#      SRCVAR = "LASTANDT"
+#    )
+#  )
+#  
+#  adtte <- derive_param_tte(
+#    dataset_adsl = adsl,
+#    start_date = RANDDT,
+#    event_conditions = list(pd_nact_event, death_nact_event),
+#    censor_conditions = list(lasta_nact_censor, rand_censor),
+#    source_datasets = list(adsl = adsl),
+#    set_values_to = vars(PARAMCD = "PFSNACT", PARAM = "Progression Free Survival prior to NACT")
+#  )
+
+## ----eval=TRUE----------------------------------------------------------------
+adtte <- adtte %>%
+  derive_vars_duration(
+    new_var = AVAL,
+    start_date = STARTDT,
+    end_date = ADT
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adtte
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  adtte_months <- adtte %>%
+#    derive_vars_duration(
+#      new_var = AVAL,
+#      start_date = STARTDT,
+#      end_date = ADT,
+#      out_unit = "months"
+#    )
+
+## ----eval=TRUE----------------------------------------------------------------
+adtte <- adtte %>%
+  derive_var_obs_number(
+    by_vars = vars(STUDYID, USUBJID),
+    order = vars(PARAMCD),
+    check_type = "error"
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(adtte)
+
+## ----eval=TRUE----------------------------------------------------------------
+adtte <- adtte %>%
+  derive_vars_merged(
+    dataset_add = adsl,
+    new_vars = vars(ARMCD, ARM, ACTARMCD, ACTARM, AGE, SEX),
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+## ---- echo=FALSE--------------------------------------------------------------
+dataset_vignette(
+  adtte,
+  display_vars = vars(USUBJID, PARAMCD, CNSR, AVAL, ARMCD, AGE, SEX)
+)
+
